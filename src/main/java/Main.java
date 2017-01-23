816e007eb9becf3f37305d36c44e170e3abe8c5e
Oleg Chorpita(Mac) <Oleg_Chorpita@epam.com>
2017-01-23 13:48:41 +0200

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        String path = "/Users/olegchorpita/Downloads/UA/UA.txt";
+        File filePath = new File(path);
+        String cityUkrNamePattern = "([А-яіг]+(?:[^,][А-яіг]+)?)\\s";
+        String cityEngNamePattern = "-|\\n";
+        String cityIndexPattern = "";
+        String latitudePattern = "";
+        String longitudePattern = "";
+
+        Scanner scanner = new Scanner(filePath);
+
+        ArrayList<Model> models = new ArrayList<Model>();
+
+        while (scanner.hasNext()) {
+            System.out.println(scanner.hasNext(Pattern.compile(cityEngNamePattern)));
+        }
+    }
+}
---
+++ src/main/java/Model.java
@@ -0,0 +1,71 @@
+/**
+ * Created by olegchorpita on 1/20/17.
+ */
+public class Model {
+    private Long id;
+    private int cityIndex;
+    private String name;
+    private String internationalName;
+    private Double latitude;
+    private Double longtitude;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getCityIndex() {
+        return cityIndex;
+    }
+
+    public void setCityIndex(int cityIndex) {
+        this.cityIndex = cityIndex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInternationalName() {
+        return internationalName;
+    }
+
+    public void setInternationalName(String internationalName) {
+        this.internationalName = internationalName;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongtitude() {
+        return longtitude;
+    }
+
+    public void setLongtitude(Double longtitude) {
+        this.longtitude = longtitude;
+    }
+
+    @Override
+    public String toString() {
+        return "Model{" +
+                "id=" + id +
+                ", cityIndex=" + cityIndex +
+                ", name='" + name + '\'' +
+                ", internationalName='" + internationalName + '\'' +
+                ", latitude=" + latitude +
+                ", longtitude=" + longtitude +
+                '}';
+    }
+}
