cc81c3556896d03195ba2f0e6dbcf4a95c07b656
Oleg Chorpita(Mac) <Oleg_Chorpita@epam.com>
2017-01-30 14:04:55 +0200

    added repository

---
+++ src/main/java/com/lunapps/repository/ModelRepoository.java
@@ -0,0 +1,9 @@
+package com.lunapps.repository;
+
+import com.lunapps.model.Model;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ModelRepoository extends JpaRepository<Model, Long> {
+}
