00e9869fc82538f44d179095e4ee1820ed1bc4a0
Oleg Chorpita(Mac) <Oleg_Chorpita@epam.com>
2017-02-03 14:08:47 +0200

    refactor

--- src/main/java/com/lunapps/sevice/TransLit.java
+++ src/main/java/com/lunapps/sevice/TransLit.java
@@ -1,10 +1,12 @@
 package com.lunapps.sevice;
 
 import com.lunapps.model.Model;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 // TODO: 2/2/17
+@Service("transLitService")
 public interface TransLit {
     void transLitCyr2Ukr(List<Model> modelList);
 }
