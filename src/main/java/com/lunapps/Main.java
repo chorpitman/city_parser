d3f7bdcbdf487142a9be8ee61cf857fcce9741b5
Oleg Chorpita(Mac) <Oleg_Chorpita@epam.com>
2017-02-06 16:33:06 +0200

    refactor

--- src/main/java/com/lunapps/Main.java
+++ src/main/java/com/lunapps/Main.java
@@ -3,12 +3,15 @@ package com.lunapps;
 import com.lunapps.configuration.AppConfig;
 import com.lunapps.model.Model;
 import com.lunapps.repository.ModelRepository;
-import com.lunapps.sevice.Transliterator;
+import com.lunapps.sevice.DownloadFile;
+import com.lunapps.sevice.UnzipFIle;
+import com.lunapps.sevice.impl.DownloadFileImpl;
 import com.lunapps.sevice.impl.ScannerFileImpl;
+import com.lunapps.sevice.impl.UnzipFIleImpl;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.Collection;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -19,16 +22,16 @@ public class Main {
         final String UNZIP_DIRECTORY = "src/main/resources/";
 
         //DOWNLOAD FILE
-//        DownloadFile downloadFile = new DownloadFileImpl();
-//        downloadFile.downloadFile(DOWNLOAD_URL, DOWNLOAD_DIRECTORY);
+        DownloadFile downloadFile = new DownloadFileImpl();
+        downloadFile.downloadFile(DOWNLOAD_URL, DOWNLOAD_DIRECTORY);
 
         //UNZIP FILE
-//        UnzipFIle unzipFIle = new UnzipFIleImpl();
-//        unzipFIle.unzip(ZIP_FILE_DIRECTORY, UNZIP_DIRECTORY);
+        UnzipFIle unzipFIle = new UnzipFIleImpl();
+        unzipFIle.unzip(ZIP_FILE_DIRECTORY, UNZIP_DIRECTORY);
 
         //PARSE FILE
         ScannerFileImpl scannerfile = new ScannerFileImpl();
-        List<Model> models = scannerfile.scanPath(SCANNER_PATH);
+        Collection<Model> models = scannerfile.scanPath(SCANNER_PATH);
         scannerfile.print(models);
         int size = models.size();
         System.out.println("List size ===== " + size);
@@ -44,10 +47,9 @@ public class Main {
         System.out.println("========List size========== " + models.size());
 
         //SPRING START
-//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-//        ModelRepository cityDao = context.getBean("modelRepository", ModelRepository.class);
-//        cityDao.save(models);
-
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        ModelRepository cityDao = context.getBean("modelRepository", ModelRepository.class);
+        cityDao.save(models);
     }
 }
 
