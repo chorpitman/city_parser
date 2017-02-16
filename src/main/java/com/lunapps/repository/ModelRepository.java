7cace0cf67fedffbb74be5360a40b525b2ac4794
Oleg Chorpita(Mac) <Oleg_Chorpita@epam.com>
2017-02-16 18:33:20 +0200

    refactor

--- src/main/java/com/lunapps/repository/ModelRepository.java
+++ src/main/java/com/lunapps/repository/ModelRepository.java
@@ -1,7 +1,6 @@
 package com.lunapps.repository;
 
 import com.lunapps.model.Model;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
