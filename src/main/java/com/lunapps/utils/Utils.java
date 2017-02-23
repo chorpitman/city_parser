e962e50fc7ef4424288a4234c226cdf9d2820685
Oleg Chorpita (Mac) <chorpita@gmail.com>
2017-02-23 14:42:48 +0200

    added method which remove duplicate by coordibates

--- src/main/java/com/lunapps/utils/Utils.java
+++ src/main/java/com/lunapps/utils/Utils.java
@@ -2,9 +2,11 @@ package com.lunapps.utils;
 
 import com.lunapps.model.Model;
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 
 public class Utils {
@@ -42,5 +44,61 @@ public class Utils {
         if (CollectionUtils.isEmpty(models)) throw new IllegalArgumentException("models can not be empty or null");
         models.removeIf(model -> model.getCityUkrName().equals(NAME_NON_CYR));
 
+    }
+
+    public static List<Model> getListWithDuplicatesCoordinates(List<Model> modelsList) {
+        if (CollectionUtils.isEmpty(modelsList)) throw new IllegalArgumentException();
+        String currentCityUkrName = "";
+        String currentRegionUkrName = "";
+        double lat = 0;
+        double lon = 0;
+
+        ArrayList<Model> duplicatesList = new ArrayList<>();
+
+        for (int i = 0; i < modelsList.size(); i++) {
+            Model model = modelsList.get(i);
+            if (StringUtils.isBlank(currentCityUkrName) & StringUtils.isBlank(currentRegionUkrName)) {
+                currentCityUkrName = model.getCityUkrName();
+                currentRegionUkrName = model.getRegionCyrillicName();
+                lat = model.getLatitude();
+                lon = model.getLongitude();
+
+            } else if (Objects.equals(currentRegionUkrName, model.getRegionCyrillicName()) &
+                    Objects.equals(currentCityUkrName, model.getCityUkrName())) {
+                if (Utils.getDistanceBitween2Points(lat, lon, model.getLatitude(), model.getLongitude()) <= 1) {
+                    System.out.println("distance <= 0.5 " + model.getCityIndex());
+                    duplicatesList.add(model);
+                } else {
+                    lat = model.getLatitude();
+                    lon = model.getLongitude();
+                }
+            } else {
+                currentCityUkrName = model.getCityUkrName();
+                currentRegionUkrName = model.getRegionCyrillicName();
+                lat = model.getLatitude();
+                lon = model.getLongitude();
+            }
+        }
+//        duplicatesList.removeAll(list);
+        return duplicatesList;
+    }
+
+    public static Double getDistanceBitween2Points(Double lat1, Double lon1, Double lat2, Double lon2) {
+        final int R = 6371; // Radious of the earth
+
+        Double latDistance = toRad(lat2 - lat1);
+        Double lonDistance = toRad(lon2 - lon1);
+
+        Double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) +
+                Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+                        Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        Double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        Double distance = R * c;
+        System.out.println("The distance between two lat and long is -> " + distance);
+        return distance;
+    }
+
+    private static Double toRad(Double value) {
+        return value * Math.PI / 180;
     }
 }
