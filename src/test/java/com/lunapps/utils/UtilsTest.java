a2b28e73861ba36ce2d96210f3f85a9d25af0deb
Oleg Chorpita <chorpita@gmail.com>
2017-02-25 14:54:30 +0200

    added configuration and impl tests

--- src/test/java/com/lunapps/utils/UtilsTest.java
+++ src/test/java/com/lunapps/utils/UtilsTest.java
@@ -1,14 +1,15 @@
 package com.lunapps.utils;
 
 import com.lunapps.config.utils.BaseTestModelHelper;
-import com.lunapps.configuration.AppConfig;
+import com.lunapps.configuration.HibernateConfig;
 import com.lunapps.model.Model;
 import com.lunapps.repository.ModelRepository;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -18,28 +19,22 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
-import static junit.framework.Assert.*;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNotSame;
+import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.*;
 
-// FIXME: 2/24/17 continue implement test
 @RunWith(SpringJUnit4ClassRunner.class)
-@ActiveProfiles("tmp-data")
-@Sql(scripts = {"classpath:sql/drop.sql"})
-//@ContextConfiguration(classes = {HibernateConfig.class, TemporaryDataPopulationConfig.class})
+@ContextConfiguration(classes = {HibernateConfig.class})
 @TestPropertySource(value = "classpath:app-config-test.properties")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@Sql(scripts = {"classpath:sql/ddl.sql", "classpath:sql/insert_dnipro.sql"})
 public class UtilsTest {
+
+    @Qualifier("modelRepository")
+    @Autowired
     private ModelRepository repository;
 
-    @Before
-    public void setUp() throws Exception {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        repository = context.getBean(ModelRepository.class);
-    }
-
     @Test
-    public void should_return_count_non_cyr_entity_countNonCyrillic()  {
+    public void should_return_count_non_cyr_entity_countNonCyrillic() {
         final String NON_CYR_NAME = "non cyrillic";
         final String CYR_NAME = "Львівська область";
         final int EXPECTED_SIZE = 2;
@@ -59,7 +54,7 @@ public class UtilsTest {
     }
 
     @Test
-    public void createModelWithNonCyrCityName()  {
+    public void createModelWithNonCyrCityName() {
         final String NON_CYR_NAME = "non cyrillic";
         final String CYR_NAME = "Львівська область";
         final int EXPECTED_COUNT = 1;
@@ -79,7 +74,7 @@ public class UtilsTest {
     }
 
     @Test
-    public void should_remove_with_non_cyr_city_name_EntityWithNonCyrCityName()  {
+    public void should_remove_with_non_cyr_city_name_EntityWithNonCyrCityName() {
         final String NON_CYR_NAME = "non cyrillic";
         final String CYR_NAME = "Львівська область";
         final int EXPECTED_COUNT = 1;
@@ -100,7 +95,7 @@ public class UtilsTest {
 
     @Test
     public void should_return_model_list_of_duplicates_coordinates() {
-        final int EXPECTED_COUNT = 6;
+        final int EXPECTED_COUNT = 7;
         //GIVEN
         List<Model> duplicatesList = repository.findAll();
         List<Model> sortedDuplicatesList = Utils.sortModelByRegCityLat(duplicatesList);
