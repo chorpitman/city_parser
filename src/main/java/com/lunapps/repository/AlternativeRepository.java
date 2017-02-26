d39a4a2cdea3ab10a738745c3caf2367bfef9867
Oleg Chorpita <chorpita@gmail.com>
2017-02-26 16:13:40 +0200

    removed redundant annotation

--- src/main/java/com/lunapps/repository/AlternativeRepository.java
+++ src/main/java/com/lunapps/repository/AlternativeRepository.java
@@ -4,6 +4,5 @@ import com.lunapps.model.AlternativeModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository("alterRepository")
 public interface AlternativeRepository extends JpaRepository<AlternativeModel, Long> {
 }
--- src/main/java/com/lunapps/repository/ModelRepository.java
+++ src/main/java/com/lunapps/repository/ModelRepository.java
@@ -4,7 +4,6 @@ import com.lunapps.model.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository("modelRepository")
 public interface ModelRepository extends JpaRepository<Model, Long> {
 
 }
