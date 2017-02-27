9c952b53688c46c86005c6913678b5cb78227935
Oleg Chorpita (Mac) <chorpita@gmail.com>
2017-02-27 18:58:38 +0200

    added test for context

---
+++ src/test/java/com/lunapps/config/SpringContextTest.java
@@ -0,0 +1,58 @@
+package com.lunapps.config;
+
+import com.lunapps.configuration.AppConfig;
+import com.lunapps.sevice.impl.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {AppConfig.class})
+public class SpringContextTest {
+
+    @Autowired
+    ScannerFileImpl scannerFile;
+
+    @Autowired
+    DownloadFileImpl downloadFile;
+
+    @Autowired
+    GoogleMapsSearchImpl googleMapsSearch;
+
+    @Autowired
+    GooglePlacesSearchImpl placesSearch;
+
+    @Autowired
+    UnzipFIleImpl unzipFIle;
+
+    @Test
+    public void should_return_scanner_file() {
+        assertThat(scannerFile, instanceOf(ScannerFileImpl.class));
+    }
+
+    @Test
+    public void should_return_download_file() {
+        assertThat(downloadFile, instanceOf(DownloadFileImpl.class));
+    }
+
+    @Test
+    public void should_return_google_search() {
+        assertThat(googleMapsSearch, instanceOf(GoogleMapsSearchImpl.class));
+    }
+
+    @Test
+    public void should_return_google_places() {
+        assertThat(placesSearch, instanceOf(GooglePlacesSearchImpl.class));
+    }
+
+    @Test
+    public void should_return_unzip() {
+        assertThat(unzipFIle, instanceOf(UnzipFIleImpl.class));
+    }
+}
+
