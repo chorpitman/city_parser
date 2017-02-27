8dd1d95feb53dea51ccc0838369b9fa5183ea81e
Oleg Chorpita (Mac) <chorpita@gmail.com>
2017-02-27 17:46:19 +0200

    improve test

--- src/test/java/com/lunapps/utils/UtilsTest.java
+++ src/test/java/com/lunapps/utils/UtilsTest.java
@@ -1,13 +1,11 @@
 package com.lunapps.utils;
 
-import com.lunapps.config.utils.BaseTestModelHelper;
 import com.lunapps.configuration.HibernateConfig;
 import com.lunapps.model.Model;
 import com.lunapps.repository.ModelRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
@@ -15,11 +13,9 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
-import static junit.framework.Assert.assertTrue;
 import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -29,84 +25,108 @@ import static org.junit.Assert.*;
 @Sql(scripts = {"classpath:sql/ddl.sql", "classpath:sql/insert_dnipro.sql"})
 public class UtilsTest {
 
-    @Qualifier("modelRepository")
     @Autowired
     private ModelRepository repository;
 
     @Test
-    public void should_return_count_non_cyr_entity_countNonCyrillic() {
-        final String NON_CYR_NAME = "non cyrillic";
-        final String CYR_NAME = "Львівська область";
-        final int EXPECTED_SIZE = 2;
-        final int EXPECTED_COUNT = 1;
+    public void should_return_count_non_cyr_entity_countNonCyrillicModel() {
         //GIVEN
-        Model non_cyr = BaseTestModelHelper.getModel(NON_CYR_NAME);
-        Model cyr = BaseTestModelHelper.getModel(CYR_NAME);
-        ArrayList<Model> list = new ArrayList<>();
-        list.addAll(Arrays.asList(non_cyr, cyr));
+        final int EXPECTED_COUNT = 2;
+        List<Model> all = repository.findAll();
+
         //WHEN
-        int count = Utils.countNonCyrillicModel(list);
+        int countNonCyrillicModel = Utils.countNonCyrillicModel(all);
+
         //THEN
-        assertNotNull(non_cyr);
-        assertNotNull(cyr);
-        assertEquals(list.size(), EXPECTED_SIZE);
-        assertEquals(count, EXPECTED_COUNT);
+        assertNotNull(countNonCyrillicModel);
+        assertEquals(EXPECTED_COUNT, countNonCyrillicModel);
     }
 
     @Test
-    public void createModelWithNonCyrCityName() {
-        final String NON_CYR_NAME = "non cyrillic";
-        final String CYR_NAME = "Львівська область";
-        final int EXPECTED_COUNT = 1;
+    public void should_return_model_with_non_cyr_city_name_getNonCyrCityNameModels() {
         //GIVEN
-        Model non_cyr = BaseTestModelHelper.getModel(NON_CYR_NAME);
-        Model cyr = BaseTestModelHelper.getModel(CYR_NAME);
-        ArrayList<Model> list = new ArrayList<>();
-        list.addAll(Arrays.asList(non_cyr, cyr));
+        final int EXPECTED_COUNT = 2;
+        List<Model> modelsList = repository.findAll();
+
         //WHEN
-        Collection<Model> listModelWithNonCyrCityName = Utils.getNonCyrCityNameModels(list);
+        Collection<Model> nonCyrCityNameModels = Utils.getNonCyrCityNameModels(modelsList);
+
         //THEN
-        assertNotNull(cyr);
-        assertNotNull(non_cyr);
-        assertNotSame(list, listModelWithNonCyrCityName);
-        assertEquals(EXPECTED_COUNT, listModelWithNonCyrCityName.size());
-        assertTrue(listModelWithNonCyrCityName.contains(non_cyr));
+        assertNotNull(modelsList);
+        assertNotNull(nonCyrCityNameModels);
+        assertEquals(EXPECTED_COUNT, nonCyrCityNameModels.size());
     }
 
     @Test
-    public void should_remove_with_non_cyr_city_name_EntityWithNonCyrCityName() {
-        final String NON_CYR_NAME = "non cyrillic";
-        final String CYR_NAME = "Львівська область";
-        final int EXPECTED_COUNT = 1;
+    public void should_remove_with_non_cyr_city_name_removeNonCyrCityNameModels() {
         //GIVEN
-        Model non_cyr = BaseTestModelHelper.getModel(NON_CYR_NAME);
-        Model cyr = BaseTestModelHelper.getModel(CYR_NAME);
-        ArrayList<Model> list = new ArrayList<>();
-        list.addAll(Arrays.asList(non_cyr, cyr));
+        final int EXPECTED_COUNT = 11;
+        final int EXPECTED_COUNT_AFTER_REMOVE_NON_CYR = 9;
+
+        List<Model> modelsList = repository.findAll();
+        ArrayList<Model> copyModelsList = new ArrayList<>(modelsList);
+
         //WHEN
-        Utils.removeNonCyrCityNameModels(list);
+        Utils.removeNonCyrCityNameModels(modelsList);
+
         //THEN
-        assertNotNull(non_cyr);
-        assertNotNull(cyr);
-        assertEquals(list.size(), EXPECTED_COUNT);
-        assertEquals(list.get(0).getCityUkrName(), CYR_NAME);
-        assertEquals(list.get(0).getId(), cyr.getId());
+        assertNotNull(modelsList);
+        assertNotNull(copyModelsList);
+        assertNotSame(copyModelsList, modelsList);
+        assertEquals(EXPECTED_COUNT, copyModelsList.size());
+        assertEquals(EXPECTED_COUNT_AFTER_REMOVE_NON_CYR, modelsList.size());
     }
 
     @Test
     public void should_return_model_list_of_duplicates_coordinates() {
-        final int EXPECTED_COUNT = 7;
         //GIVEN
-        List<Model> duplicatesList = repository.findAll();
-        List<Model> sortedDuplicatesList = Utils.sortModelByRegCityLat(duplicatesList);
+        final int EXPECTED_COUNT = 7;
+        List<Model> modelList = repository.findAll();
+        List<Model> sortedDuplicatesList = Utils.sortModelByRegCityLat(modelList);
+
         //WHEN
         List<Model> foundListWithDuplicates = Utils.getModelsWithDuplicatesCoordinates(sortedDuplicatesList);
+
         //THEN
-        assertNotNull(duplicatesList);
+        assertNotNull(modelList);
         assertNotNull(sortedDuplicatesList);
         assertNotNull(foundListWithDuplicates);
-        assertNotSame(duplicatesList, sortedDuplicatesList);
+        assertNotSame(modelList, sortedDuplicatesList);
         assertNotSame(sortedDuplicatesList, foundListWithDuplicates);
         assertEquals(EXPECTED_COUNT, foundListWithDuplicates.size());
+    }
+
+    @Test
+    public void should_sort_by_reg_city_lat_sortModelByRegCityLat() {
+        //GIVEN
+        List<Model> modelList = repository.findAll();
+        List<Model> copyModelList = new ArrayList<>(modelList);
+
+        //WHEN
+        Utils.sortModelByRegCityLat(modelList);
+        //THEN
+        assertNotNull(modelList);
+        assertNotNull(copyModelList);
+        assertNotSame(modelList, copyModelList);
+    }
+
+    @Test
+    public void shold_return_distance_between_2_point_getDistanceBetween2Points() {
+        //GIVEN
+        List<Model> modelList = repository.findAll();
+        Double lat = modelList.get(0).getLatitude();
+        Double lon = modelList.get(0).getLongitude();
+        Double lat1 = modelList.get(1).getLatitude();
+        Double lon1 = modelList.get(1).getLongitude();
+
+        //WHEN
+        Double distanceBetween2Points = Utils.getDistanceBetween2Points(lat, lon, lat1, lon1);
+        //THEN
+        assertNotNull(modelList);
+        assertNotNull(lat);
+        assertNotNull(lon);
+        assertNotNull(lat1);
+        assertNotNull(lon1);
+        assertTrue(distanceBetween2Points > 169);
     }
 }
